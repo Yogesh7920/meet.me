397d14e9353d025593368b0b71ca435e5769ef51
Rajeev Goyal <rajeevgoyal@Rajeevs-MacBook-Pro.local>
2021-10-14 23:49:50 +0530

    inital commit

---
+++ Dashboard/Client/SessionManagement/ClientSessionManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Dashboard.Client.SessionManagement 
+{
+    public class NotImplementedErr : Exception
+    {
+        public NotImplementedErr() : base(String.Format("Not Implemented")) { }
+    }
+    class ClientSessionManager : IUXClientSM
+    {
+        /// <summary>
+        /// Returns the credentials required to 
+        /// Join or start the meeting
+        /// </summary>
+        /// <returns> A MeetingCredentials Object </returns>
+        public MeetingCredentials GetPortsAndIPAddress()
+        {
+            throw new NotImplementedErr();
+        }
+
+        /// <summary>
+        /// Adds a user to the meeting.
+        /// </summary>
+        /// <param name="ipAddress"> IP Address of the meeting. </param>
+        /// <param name="ports"> port number. </param>
+        /// <param name="username"> Name of the user. </param>
+        /// <returns> Boolean denoting the success or failure whether the user was added. </returns>
+        public bool AddClient(string ipAddress, int ports, string username)
+        {
+            throw new NotImplementedErr();
+        }
+
+        /// <summary>
+        /// Removes the user from the meeting by deleting their 
+        /// data from the session.
+        /// </summary>
+        public void RemoveClient()
+        {
+            throw new NotImplementedErr();
+        }
+
+        /// <summary>
+        /// End the meeting for all, creating and storing the summary and analytics.
+        /// </summary>
+        public void EndMeet()
+        {
+            throw new NotImplementedErr();
+        }
+
+        /// <summary>
+        /// Get the summary of the chats that were sent from the start of the
+        /// meet till the function was called.
+        /// </summary>
+        /// <returns> Summary of the chats as a string. </returns>
+        public string GetSummary()
+        {
+            throw new NotImplementedErr();
+        }
+
+        /// <summary>
+        /// Used to subcribe for any changes in the 
+        /// Session object.
+        /// </summary>
+        /// <param name="listener"> The subscriber. </param>
+        /// <param name="identifier"> The identifier of the subscriber. </param>
+        public void SubscribeSession(ISessionNotifications listener, string identifier)
+        {
+            throw new NotImplementedErr();
+        }
+
+        /// <summary>
+        /// Gather analytics of the users and messages.
+        /// </summary>
+        public void GetAnalytics()
+        {
+            // the return type will be an analytics object yet to be decided.
+            throw new NotImplementedErr();
+        }
+
+        /// <summary>
+        /// Will Notifiy UX about the changes in the Session
+        /// </summary>
+        public void NotifyUXSession()
+        {
+
+        }
+
+        public SessionData _sessionObject;
+    }
+}
---
+++ Dashboard/Server/SessionManagement/ServerSessionManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.Server.SessionManagement
+{
+    public class ServerSessionManager : ITelemetrySessionManager
+    {
+        public class NotImplementedErr : Exception
+        {
+            public NotImplementedErr() : base(String.Format("Not Implemented")) { }
+        }
+
+        /// <summary>
+        /// Subscribes to changes in the session object
+        /// </summary>
+        /// <param name="listener"> The subscriber. </param>
+        /// <param name="identifier"> The listener of the subscriber </param>
+        public void Subcribe(ITelemetryNotifications listener, string identifier)
+        {
+            throw new NotImplementedErr();
+        }
+    }
+}
