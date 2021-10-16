ba78620250d9b52d0c73cd98ec9a1cb6ba156226
SahilChaudhari23 <91545445+SahilChaudhari23@users.noreply.github.com>
2021-10-16 18:52:51 +0530

    Code clean up

--- Content/Models/SendMessageData.cs
+++ Content/Models/SendMessageData.cs
@@ -2,10 +2,9 @@ namespace Content
 {
     public class SendMessageData
     {
-        public MessageType messageType; // enum indicating file or chat
-        public string message; // message if chat, filepath if message type is File
-        public int[] receiverIds; // list of receiver ids
-        public int replyThreadId; // special value (-1) if message not part of any existing thread
-                                  // otherwise thread id of thread to which the message belongs
+        public MessageType messageType;
+        public string message; 
+        public int[] receiverIds;
+        public int replyThreadId;
     }
 }
