3360447994f5523f7fbfe2283fb5d11330d0ac99
Yuvraj Raghuvanshi <111801048@smail.iitpkd.ac.in>
2021-10-16 22:12:17 +0530

    Implemented singleton factory for Content client.

--- Content/Client/ContentClientFactory.cs
+++ Content/Client/ContentClientFactory.cs
@@ -4,15 +4,47 @@ namespace Content
     public class ContentClientFactory
     {
         private ContentClientFactory() { }
-        private static IContentClient _instance;
-
+        private static ContentClient instance;
+        private static readonly object _lock = new object();
+        
+        private static void createInstance()
+        {
+            // acquire lock before creating object to ensure thread saftey
+            lock (_lock)
+            {
+                // check to ensure a thread acquiring lock just after
+                // creation by another thread doesn't create a new object
+                if (instance == null)
+                {
+                    instance = new ContentClient();
+                }
+            }
+        }
+        /// <summary>
+        /// Sets user ID of instance
+        /// if the instance doesn't exist yet, it is created
+        /// </summary>
+        /// <param name="userId">User ID of the client</param>
         public static void setUser(int userId)
         {
-            throw new NotImplementedException();
+            if (instance == null)
+            {
+                createInstance();
+            }
+            instance.UserId = userId;
         }
+        /// <summary>
+        /// Returns singleton instance of an implementation of the
+        /// IContentClient interface in a thread safe way
+        /// </summary>
+        /// <returns>Object that implements the IContentClient interface</returns>
         public static IContentClient getInstance()
         {
-            throw new NotImplementedException();
+            if (instance == null)
+            {
+                createInstance();
+            }
+            return instance;
         }
     }
 }
