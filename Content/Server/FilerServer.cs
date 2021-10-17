d0cf326a191b88f63ffdc32dcbe5774cbd3b7257
Sameer <92028250+sameer38@users.noreply.github.com>
2021-10-17 11:23:01 +0530

    Added FileServer and ChatServer

---
+++ Content/Server/FilerServer.cs
@@ -0,0 +1,15 @@
+﻿namespace Content
+{
+    internal class FilerServer
+    {
+        private string SaveFile(ReceiveMessageData messageData)
+        {
+            return "";
+        }
+
+        private ReceiveMessageData FetchFile(string file)
+        {
+            return new ReceiveMessageData();
+        }
+    }
+}
