d67c84c4bbc4519ac3f1617bfdca158a23b9999e
Sameer <92028250+sameer38@users.noreply.github.com>
2021-10-17 11:16:35 +0530

    code cleanup

--- Content/server/ContentServer.cs
+++ Content/server/ContentServer.cs
@@ -7,28 +7,19 @@ namespace Content
         private List<IContentListener> subscribers;
         private List<Thread> allMessages;
 
-        /// <summary>
-        /// Add a new subscriber to the list of subscribers
-        /// </summary>
-        /// <param name="subscriber">IContentListener implementation provided by the subscriber</param>
+        /// <inheritdoc />
         public void SSubscribe(IContentListener subscriber)
         {
             subscribers.Add(subscriber);
         }
 
-        /// <summary>
-        /// Get all the messages sent
-        /// </summary>
-        /// <returns>List of Thread objects</returns>
+        /// <inheritdoc />
         public List<Thread> SGetAllMessages()
         {
             return allMessages;
         }
 
-        /// <summary>
-        /// Sends all the messages to the client of the user with user id = userId
-        /// </summary>
-        /// <param name="userId">user id of the user to which messages needs to be sent</param>
+        /// <inheritdoc />
         public void SSendAllMessagesToClient(int userId)
         {
             return;
