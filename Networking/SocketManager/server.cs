10e925bffcd0ac4ab252d27bbc42ccb74f5a7ef9
TausifIqbal <111801044@smail.iitpkd.ac.in>
2021-10-26 22:22:22 +0530

    added server.cs

---
+++ Networking/SocketManager/server.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections;
+using System.Net;
+
+/// <summary>
+/// This file contains the class Server which will be running on the server.
+/// and accepts client request.
+/// </summary>
+/// <author>Tausif Iqbal </author>
+namespace Networking
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            int port = FreeTcpPort();
+            TcpListener serverSocket = new TcpListener(System.Net.IPAddress.Loopback, port);
+            serverSocket.Start();
+            TcpClient clientSocket = default(TcpClient);
+            Console.WriteLine("Server has started with ip = " +
+            IPAddress.Parse(((IPEndPoint)serverSocket.LocalEndpoint).Address.ToString()) +
+            " and port number = " + ((IPEndPoint)serverSocket.LocalEndpoint).Port.ToString());
+
+
+            while (true)
+            {
+                clientSocket = serverSocket.AcceptTcpClient();
+
+            }
+
+        }
+
+        /// <summary>
+        /// scan for free Tcp port.
+        /// </summary>
+        /// <returns>Free port </returns>
+        private static int FreeTcpPort()
+        {
+            TcpListener tcp = new TcpListener(System.Net.IPAddress.Loopback, 0);
+            tcp.Start();
+            int port = ((IPEndPoint)tcp.LocalEndpoint).Port;
+            tcp.Stop();
+            return port;
+        }
+        
+
+    }
+}
