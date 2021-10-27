2f9db777146f0f4dd0309c7924f6d5caf6e8d097
pulavartivinay <pulavartivinay@gmail.com>
2021-10-28 00:25:41 +0530

    Implemented startMeet and endMeet commands but backend connections are left

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Server
 {
@@ -6,7 +7,30 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while(true)
+            {
+                //To read a command from the Terminal
+                string s = Console.ReadLine();
+                //if the input command entered is startMeet, will call the GetPortsAndIPAddress function which creates the meeting and 
+                //returns the object which contains IPAddress and Port of the meeting
+                if(s == "startMeet")
+                {
+                    MeetingCredentials m = GetPortsAndIPAddress();
+                    console.writeline(m.ipaddress + ":" + m.port);
+                    Console.WriteLine("Meeting has started by Host");
+                }
+                //if the input command entered is endMeet, will call the EndMeet function which ends the meeting
+                else if (s == "endMeet")
+                {
+                    EndMeet();
+                    Console.WriteLine("Meeting has ended by Host");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Command. Try startMeet or endMeet commands!!!");
+                }
+
+            }
         }
     }
 }
