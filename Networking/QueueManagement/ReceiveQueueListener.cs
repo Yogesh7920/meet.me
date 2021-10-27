3c376fbc6bceabf925640c8fa886e9714452cfb0
Sai Vamsi Alisetti <52487689+Vamsi995@users.noreply.github.com>
2021-10-27 11:14:39 +0530

    Queue Management: Implementation of Receive Queue Listener

---
+++ Networking/QueueManagement/ReceiveQueueListener.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Networking
+{
+    public class ReceiveQueueListener
+    {
+        private IQueue _receieveQueue;
+        private Dictionary<string, INotificationHandler> _notificationHandlers;
+
+        public ReceiveQueueListener(Dictionary<string , INotificationHandler> notificationHandlers)
+        {
+            _receieveQueue = new Queue();
+            this._notificationHandlers = notificationHandlers;
+        }
+
+        public void ListenQueue()
+        {
+            while (!(_receieveQueue.IsEmpty()))
+            {
+                Packet Item = _receieveQueue.Dequeue();
+                string Data = Item.SerializedData;
+                string ModuleIdentifier = Item.ModuleIdentifier;
+
+                if (_notificationHandlers.ContainsKey(ModuleIdentifier))
+                {
+                    INotificationHandler Handler = _notificationHandlers[ModuleIdentifier];    
+                    Handler.OnDataReceived(Data);
+                }
+                else
+                {
+                    throw new Exception("Handler does not exist");
+                }
+            }        
+        }
+    }
+}
