9602d0ee986722d565d6acb69a55b77ae3d6eb3a
bruise_88 <sidshah1036.ss@gmail.com>
2021-10-28 14:23:42 +0530

    Created a folder structure for Dashboard Testing

---
+++ Testing/Dashboard/SummaryLogic/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Dashboard.SummaryLogic.Testing
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
