4f5bbaff1dfb46bb7aaeb901db25e5361281b6e6
bruise_88 <sidshah1036.ss@gmail.com>
2021-10-28 16:09:33 +0530

    minor fix

--- Dashboard/Server/SessionManagement/IUXServerSessionManager.cs
+++ Dashboard/Server/SessionManagement/IUXServerSessionManager.cs
@@ -6,16 +6,13 @@ using System.Threading.Tasks;
 
 namespace Dashboard.Server.SessionManagement
 {
-    interface IUXServerSessionManager
+    public interface IUXServerSessionManager
     {
-        public interface IUXServerSessionManager
-        {
-            /// <summary>
-            /// Returns the credentials required to 
-            /// Join or start the meeting
-            /// </summary>
-            /// <returns> A MeetingCredentials Object </returns>
-            MeetingCredentials GetPortsAndIPAddress();
-        }
+        /// <summary>
+        /// Returns the credentials required to 
+        /// Join or start the meeting
+        /// </summary>
+        /// <returns> A MeetingCredentials Object </returns>
+        MeetingCredentials GetPortsAndIPAddress();
     }
 }
