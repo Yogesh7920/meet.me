6301ccf14f19053fb8fc5ef3fc70a70f699003ff
Gurunadh <gurunadh.pg123@gmail.com>
2021-10-28 12:42:46 +0530

    server side intial implementation

--- Whiteboard/Server/ServerBoardCommunicator.cs
+++ Whiteboard/Server/ServerBoardCommunicator.cs
@@ -2,37 +2,73 @@
  * Owned By: Gurunadh Pachappagari
  * Created By: Gurunadh Pachappagari
  * Date Created: 13 Oct 2021
- * Date Modified: 13 Oct 2021
+ * Date Modified: 28 Oct 2021
 **/
 
 using System;
 using System.Collections.Generic;
+using Networking;
 
 namespace Whiteboard
 {
     /// <summary>
     /// Bridge the gap between Server side White Board Modules and Networking module
     /// </summary>
-    public class ServerBoardCommunicator : IServerMessageListener, IServerBoardCommunicator
+    public sealed class ServerBoardCommunicator : INotificationHandler , IServerBoardCommunicator
     {
+
+
+        private static ServerBoardCommunicator instance = null;
+        private static ISerializer serializer;
+        private static ICommunicator communicator;
+        private static string moduleIdentifier = "Whiteboard";
         /// <summary>
-        /// 1. sends/broadcasts the xml string using IServerCommunicator.send/broadcast
-        /// 2. deserializes the xml string to Board server shape and passes to server state manager
+        /// private constructor for a singleton
         /// </summary>
-        /// <param name="data"> xml string received from server side</param>
-        public void OnMessageReceived(string data)
+        private ServerBoardCommunicator() { }
+
+        /// <summary>
+        /// instance getter
+        /// </summary>
+        public static ServerBoardCommunicator Instance
         {
-            throw new NotImplementedException();
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ServerBoardCommunicator();
+                    serializer = new Serializer();
+                    communicator = new Communicator();
+                    communicator.Subscribe(moduleIdentifier, instance);
+                }
+                return instance;
+            }
+        }
+
+        public void OnDataReceived(string data)
+        {
+            string c_data = data;
+            BoardServerShape deserializedShape = serializer.Deserialize<BoardServerShape>(data);
+            // for now broadcasting the object to all clients 
+            communicator.Send(data, moduleIdentifier);
         }
 
         /// <summary>
-        /// serializes the shape objects and passes it to IServerCommunicator
+        /// serializes the shape objects and passes it to communicator.send()
         /// </summary>
-        /// <param name="serverShapes"> the object to be passed to clients</param>
-        /// <param name="clientID"> client id to whom to send these objects to</param>
-        public void Send(List<BoardServerShape> clientUpdate, string clientID) 
-        { 
-            throw new NotImplementedException();
+        /// <param name="clientUpdate"> the object to be passed to server</param>
+        public void Send(BoardServerShape clientUpdate, string clientId = "all")
+        {
+            string xml_obj = serializer.Serialize(clientUpdate);
+            if (clientId == "all")
+            {
+                communicator.Send(xml_obj, moduleIdentifier);
+            }
+            else 
+            {
+                communicator.Send(xml_obj, moduleIdentifier, clientId);
+            }
+
         }
 
     }
