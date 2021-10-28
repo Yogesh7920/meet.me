9648e7fee030662ce08c002fc138e31a8f22ddf8
pulavartivinay <pulavartivinay@gmail.com>
2021-10-29 01:03:01 +0530

    Completed the Task

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,11 +3,14 @@ using System.Diagnostics;
 
 namespace Server
 {
+    using Dashboard;
+    using Dashboard.Client.SessionManagement;
     class Program
     {
         static void Main(string[] args)
         {
-            while(true)
+            IUXClientSM UXClientSM = SMFactory.GetClientSessionManager();
+            while (true)
             {
                 // To read a command from the Terminal
                 string Command = Console.ReadLine();
@@ -15,14 +18,14 @@ namespace Server
                 {
                     // if the input command entered is startMeet, will call the GetPortsAndIPAddress function which creates the meeting and 
                     // returns the object which contains IPAddress and Port of the meeting
-                    MeetingCredentials Meeting = GetPortsAndIPAddress();
-                    Console.WriteLine(Meeting.ipaddress + " : " + Meeting.port);
+                    MeetingCredentials Meeting = UXClientSM.GetPortsAndIPAddress();
+                    Console.WriteLine(Meeting.ipAddress + " : " + Meeting.ports);
                     Console.WriteLine("Meeting has started by Host");
                 }
                 else if (Command == "EndMeet")
                 {
                     // if the input command entered is endMeet, will call the EndMeet function which ends the meeting
-                    EndMeet();
+                    UXClientSM.EndMeet();
                     Console.WriteLine("Meeting has ended by Host");
                 }
                 else
