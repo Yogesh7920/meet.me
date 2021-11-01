3a05b2e86b4a627d87278adae0996ef70ddc0269
ecstasy-repos <parmanandedx1234@gmail.com>
2021-11-01 23:19:28 +0530

    Used proper param syntax and declared class in separate file

--- Dashboard/Server/Persistence/ITelemetryPersistence.cs
+++ Dashboard/Server/Persistence/ITelemetryPersistence.cs
@@ -17,40 +17,23 @@ namespace Dashboard.Server.Persistence
         /// <summary>
                 /// save the UserCountVsTimeStamp, UserIdVsChatCount, InsincereMember data as png after each session.
         /// </summary>
-        /// <params> takes IntraSessionData from Telemetry.
+        /// <param name="intraSessionData"> takes IntraSessionData from Telemetry. </param>
 	    bool save(IntraSessionData intraSessionData);
 
         /// <summary>
                  /// append the ServerData into a file after each session end
         /// </summary>
-        /// <params> takes ServerData from Telemetry
+        /// <param name="serverData"> takes ServerData from Telemetry to be saved into text file </param> 
+        /// <returns>Returns true if saved successfully else returns false</returns>
         bool saveServerData(ServerData serverData);
 
         /// <summary>
                  /// retrives the ServerData after end of all of the sessions.
         /// </summary>
+        /// <returns>returns List of SeverData</returns>
         List<ServerData> retriveAllSeverData();
 
     }
 
-    public class TelemetryPersistence : ITelemetryPersistence
-    {
-        public override bool save(IntraSessionData intraSessionData)
-        {
-
-        }
-
-        public override bool saveServerData(ServerData serverData)
-        {
-
-        }
-
-        public override List<ServerData> retriveAllSeverData()
-        {
-
-        }
-
-
-    }
 
 }
