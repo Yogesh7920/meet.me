da69403475f839f68724a200dbc9f3c10e4d6edf
ecstasy-repos <parmanandedx1234@gmail.com>
2021-11-02 15:12:24 +0530

    Update SummaryPersistence.cs

--- Dashboard/Server/Persistence/SummaryPersistence.cs
+++ Dashboard/Server/Persistence/SummaryPersistence.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace Dashboard.Server.Persistence
 {
-    /// <summary>
-    /// saves the summary of the session into a summary file
-    /// </summary>
-    /// <param name="sessionId"> takes sessionId to denote the name of the file to which we need to save. </param>
-    /// <param name="message"> takes message string that need to be saved </param>
-    /// <returns> return true if succesfully saved else return false </returns>
+   
     public class SummaryPersistence : ISummaryPersistence
     {
+        /// <summary>
+        /// saves the summary of the session into a summary file
+        /// </summary>
+        /// <param name="sessionId"> takes sessionId to denote the name of the file to which we need to save. </param>
+        /// <param name="message"> takes message string that need to be saved </param>
+        /// <returns> return true if succesfully saved else return false </returns>
         public override bool saveSummary(String meetingId, string message)
         {
 
