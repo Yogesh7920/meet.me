71ccb1630fc78dbf35d1f212d0acdecdd249f05a
Sai Vamsi Alisetti <52487689+Vamsi995@users.noreply.github.com>
2021-11-03 01:45:12 +0530

    Queue Management: Unit Testing First Draft

--- Testing/Networking/QueueManagement.cs
+++ Testing/Networking/QueueManagement.cs
@@ -49,6 +49,7 @@ namespace Testing.Networking
             int moduleIndex;
             string tempData = "tempData";
             
+            // Creating packets with random moduleIdentifiers
             for (int i = 0; i < _testPackets.Capacity; i++)
             {
                 moduleIndex = random.Next(0,4);
@@ -62,6 +63,7 @@ namespace Testing.Networking
         [TearDown]
         public void TearDown()
         {
+            // Dereference objects after the end of each test
             _moduleIdentifiers = null;
             _queue = null;
             _testPackets = null;
@@ -104,6 +106,8 @@ namespace Testing.Networking
         public void Enqueue_MultiplePackets_SizeShouldMatch()
         {
             int size = 0;
+            
+            // Running enqueue from different threads
             Task thread1 = Task.Run(() =>
             {
                 for (int i = 0; i < _testPackets.Capacity/2; i++)
@@ -220,6 +224,30 @@ namespace Testing.Networking
             string expectedMessage = "Adding Queue to MultiLevelQueue Failed!";
             Assert.AreEqual(expectedMessage, registerEx.Message);
         }
+        
+        [Test]
+        public void RegisterModule_IncorrectPriority_ThrowsException()
+        {
+            string moduleId = "A";
+            int priority = -1;
+            
+            AggregateException ex = Assert.Throws<AggregateException>(() =>
+            {
+                Task thread = Task.Run(() =>
+                {
+                    _queue.RegisterModule(moduleId, priority);
+                   
+                });
+
+                Task.WaitAll(thread);
+            });
+            
+            ReadOnlyCollection<Exception> innerEx = ex.InnerExceptions;
+            Exception registerEx = innerEx.ElementAt(0);
+
+            string expectedMessage = "Priority should be positive integer";
+            Assert.AreEqual(expectedMessage, registerEx.Message);
+        }
 
         [Test]
         public void Dequeue_QueueIsEmpty_ThrowsException()
@@ -312,8 +340,7 @@ namespace Testing.Networking
             size = _queue.Size();
             Assert.AreEqual(0, size);
         }
-
-
+        
         [Test]
         public void Peek_SinglePacket_ShouldPeekSamePacket()
         {
@@ -335,8 +362,130 @@ namespace Testing.Networking
 
             Task.WaitAll(thread1, thread2);
         }
-        // [Test]
-        // public void 
         
+        [Test]
+        public void Dequeue_CheckingOrder_ReturnsProperOrder()
+        {
+            const string moduleId1 = "X";
+            const string xData1 = "xData1";
+            const string xData2 = "xData2";
+            const string xData3 = "xData3";
+
+            const string moduleId2 = "Y";
+            const string yData1 = "yData1";
+            const string yData2 = "yData2";
+
+            _queue.RegisterModule(moduleId1, 2);
+            _queue.RegisterModule(moduleId2,  1);
+            
+            Packet xPacket1 = new Packet(moduleId1, xData1);
+            Packet xPacket2 = new Packet(moduleId1, xData2);
+            Packet xPacket3 = new Packet(moduleId1, xData3);
+            
+            Packet yPacket1 = new Packet(moduleId2, yData1);
+            Packet yPacket2 = new Packet(moduleId2, yData2);
+            
+
+            Task thread1 = Task.Run(() =>
+            {
+                _queue.Enqueue(xPacket1);
+                _queue.Enqueue(xPacket2);
+                _queue.Enqueue(xPacket3);
+
+            });
+
+            Task thread2 = Task.Run(() =>
+            {
+                _queue.Enqueue(yPacket1);
+                _queue.Enqueue(yPacket2);
+            });
+
+            Task thread3 = Task.Run(() =>
+            {
+                Packet p1 = _queue.Dequeue();
+                Assert.AreEqual(moduleId1, p1.ModuleIdentifier);
+                Assert.AreEqual(xData1, p1.SerializedData);
+
+                Packet p2 = _queue.Dequeue();
+                Assert.AreEqual(moduleId1, p2.ModuleIdentifier);
+                Assert.AreEqual(xData2, p2.SerializedData);
+
+                Packet p3 = _queue.Dequeue();
+                Assert.AreEqual(moduleId2, p3.ModuleIdentifier);
+                Assert.AreEqual(yData1, p3.SerializedData);
+                
+                Packet p4 = _queue.Dequeue();
+                Assert.AreEqual(moduleId1, p4.ModuleIdentifier);
+                Assert.AreEqual(xData3, p4.SerializedData);
+                
+                Packet p5 = _queue.Dequeue();
+                Assert.AreEqual(moduleId2, p5.ModuleIdentifier);
+                Assert.AreEqual(yData2, p5.SerializedData);
+            });
+
+            Task.WaitAll(thread1, thread2, thread3);
+        }
+
+        [Test]
+        public void Dequeue_RegisteringModuleAmidst_ShouldNotAlterDequeueFunctionality()
+        {
+            const string moduleId = "S";
+            const string data = "screenData";
+
+            const string moduleId2 = "C";
+            const string data2 = "chatData";
+
+            const string newModuleId = "X";
+            const string newData = "xData";
+            int newPriority = 5;
+
+            Packet packet1 = new Packet(moduleId, data);
+            Packet packet2 = new Packet(moduleId2, data2);
+            Packet packet3 = new Packet(newModuleId, newData);
+            
+            Task thread1 = Task.Run(() =>
+            {
+                _queue.Enqueue(packet1);
+            });
+
+            Task thread2 = Task.Run(() =>
+            {
+                _queue.Enqueue(packet2);
+            });
+            
+            Task.WaitAll(thread1, thread2);
+
+            Task thread3 = Task.Run(() =>
+            {
+                Packet p1 = _queue.Dequeue();
+                Assert.AreEqual(moduleId, p1.ModuleIdentifier);
+                Assert.AreEqual(data, p1.SerializedData);
+            });
+
+            Task thread4 = Task.Run(() =>
+            {
+                _queue.RegisterModule(newModuleId, newPriority);
+            });
+            
+            Task.WaitAll(thread3, thread4);
+
+            Task thread5 = Task.Run(() =>
+            {
+                _queue.Enqueue(packet3);
+            });
+
+            Task thread6 = Task.Run(() =>
+            {
+                Packet p2 = _queue.Dequeue();
+                Assert.AreEqual(moduleId2, p2.ModuleIdentifier);
+                Assert.AreEqual(data2, p2.SerializedData);
+
+                Packet p3 = _queue.Dequeue();
+                Assert.AreEqual(newModuleId, p3.ModuleIdentifier);
+                Assert.AreEqual(newData, p3.SerializedData);
+            });
+
+            Task.WaitAll(thread5, thread6);
+        }
     }
 }
