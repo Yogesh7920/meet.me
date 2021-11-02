16ad9a9442ea10f1637dcd58359b1f08723f76f9
Sai Vamsi Alisetti <52487689+Vamsi995@users.noreply.github.com>
2021-11-02 13:20:11 +0530

    Queue Management: Interface Comments according to guidelines

--- Networking/QueueManagement/IQueue.cs
+++ Networking/QueueManagement/IQueue.cs
@@ -5,11 +5,14 @@
         /// <summary>
         /// Register Module into the multilevel queue
         /// </summary>
+        /// <param name="moduleId">Unique Id for module</param>
+        /// <param name="priority">Priority Number indicating the weight to be given to the module</param>
         public void RegisterModule(string moduleId, int priority);
         
         /// <summary>
         /// Size of the queue
         /// </summary>
+        /// <returns>The number of packets the queue holds</returns>
         public int Size();
         
         /// <summary>
@@ -25,16 +28,19 @@
         /// <summary>
         /// Dequeues an item from the queue and returns the item
         /// </summary>
+        /// <returns>Returns the dequeued packet from the queue</returns>
         public Packet Dequeue(); 
         
         /// <summary>
         /// Peeks into the first element of the queue
         /// </summary>
+        /// <returns>Returns the peeked packet from the queue</returns>
         public Packet Peek(); 
         
         /// <summary>
         /// Checks if the queue is empty or not
         /// </summary>
+        /// <returns>true if queue is empty and false otherwise</returns>
         public bool IsEmpty(); 
     }
 }
