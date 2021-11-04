7994ee12084c2d504a40750b66cff30e02fa7808
Sameer <111801038@smail.iitpkd.ac.in>
2021-11-04 13:56:15 +0530

    handling chats

--- Content/Server/ChatServer.cs
+++ Content/Server/ChatServer.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson;
+using System;
 
 namespace Content
 {
@@ -13,10 +14,42 @@ namespace Content
 
         public MessageData Receive(MessageData messageData)
         {
+            switch (messageData.Event)
+            {
+                case MessageEvent.NewMessage:
+                    return Store(messageData);
+
+                case MessageEvent.Star:
+                    return Star(messageData.MessageId);
+
+                case MessageEvent.Update:
+                    return Update(messageData.MessageId, messageData);
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private MessageData Update(ObjectId id, MessageData messageData)
+        {
+            contentDatabase.UpdateMessageData(id, messageData);
+            return messageData;
+        }
+
+        private MessageData Star(ObjectId id)
+        {
+            MessageData messageData = Fetch(id);
+            messageData.Starred = !messageData.Starred;
+            contentDatabase.UpdateMessageData(id, messageData);
+            return messageData;
+        }
+
+        private MessageData Store(MessageData messageData)
+        {
             return contentDatabase.Store(messageData);
         }
 
-        public MessageData Fetch(ObjectId messageId)
+        private MessageData Fetch(ObjectId messageId)
         {
             return contentDatabase.RetrieveMessage(messageId);
         }
