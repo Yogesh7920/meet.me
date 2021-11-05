6b2aacc5f936d26546d5901dd1288243053f0d70
Yuvraj Raghuvanshi <111801048@smail.iitpkd.ac.in>
2021-11-05 19:17:56 +0530

    Add Serializable attribute to models and parameterized constructor to SendFileData

--- Content/Models/SendFileData.cs
+++ Content/Models/SendFileData.cs
@@ -1,15 +1,27 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.IO;
 
 namespace Content
 {
     [BsonIgnoreExtraElements]
+    [Serializable()]
     public class SendFileData
     {
-        public string fileName;
+        public SendFileData(string filepath)
+        {
+            if (!File.Exists(filepath))
+            {
+                throw new FileNotFoundException("File {0} not found", filepath);
+            }
 
-        [BsonId]
-        public ObjectId messageId;
+            fileName = Path.GetFileName(filepath);
+            fileContent = File.ReadAllBytes(filepath);
+            fileSize = fileContent.Length;
+        }
+
+        public string fileName;
 
         public byte[] fileContent;
 
