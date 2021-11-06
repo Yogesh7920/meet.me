fb80802059dea410c0e124ad17c6f3c6de2c24e1
Yuvraj Raghuvanshi <111801048@smail.iitpkd.ac.in>
2021-11-06 19:08:10 +0530

    Implemented notification handler on client to handle messages received from the network.

--- Content/Client/ContentClientNotificationHandler.cs
+++ Content/Client/ContentClientNotificationHandler.cs
@@ -1,14 +1,40 @@
 using Networking;
 using System;
+using System.Collections.Generic;
 
 namespace Content
 {
     public class ContentClientNotificationHandler : INotificationHandler
     {
+
+        private ISerializer _serializer;
+        private ContentClient _contentHandler;
+        
+        public ContentClientNotificationHandler()
+        {
+            _serializer = new Serializer();
+            _contentHandler = ContentClientFactory.getInstance() as ContentClient;
+        }
+
         /// <inheritdoc/>
         public void OnDataReceived(string data)
         {
-            throw new NotImplementedException();
+            Object deserialized = _serializer.Deserialize<Object>(data);
+
+            if (deserialized is MessageData)
+            {
+                MessageData receivedMessage = deserialized as MessageData;
+                _contentHandler.OnReceive(receivedMessage);
+            }
+
+            else if (deserialized is List<ChatContext>)
+            {
+                List<ChatContext> allMessages = deserialized as List<ChatContext>;
+                _contentHandler.Notify(allMessages);
+            }
+            
+            else
+                throw new ArgumentException("Deserialized object of unknown type");
         }
 
         /// <inheritdoc/>
