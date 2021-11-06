6808df01a691d720a32c10af6090a5b6296d8575
Sairoop Bodepudi <sairoopb@gmail.com>
2021-11-06 15:22:09 +0530

    Minor changes

--- Dashboard/Server/Summary/Summarizer.cs
+++ Dashboard/Server/Summary/Summarizer.cs
@@ -23,38 +23,45 @@ namespace Dashboard.Server.Summary
 		/// Function to get the Summary of the chat and
 		/// discussion to present in the Dashboard
 		/// </summary>
-		/// <param name="chats">
-		/// ChatContext array
+		/// <param name="chats">Array of ChatContext each of 
+		/// which would contain an array of chat messages
+		/// which would be used for the summarizer to 
+		/// generate the summary
+		/// </param>
+		/// <param name="fraction"> The fraction of the chat 
+		/// length which determines the length of summary
 		/// </param>
 		/// <returns>
-		/// String which is the summary of the discussion
-		/// </returns>
+		/// String which is the summary of the 
+		/// chat in the particular discusiion
+		/// </returns>>
 		public string GetSummary(ChatContext[] chats, double fraction = 0.2)
 		{
 			string discussionChat = "";
-			int numChats = 0;
 			foreach (ChatContext chat in chats)
 			{
 				foreach (ReceiveMessageData msg in chat.MsgList)
-				{
 					discussionChat += msg.Message;
-					numChats++;
-				}
 			}
 			return _processor.Summarize(discussionChat, fraction);
 		}
 
 		/// <summary>
-		/// Function to save the Summary of the chat and
-		/// discussion to present in the Dashboard into 
-		/// the database
+		/// Function to save the summary of the dashboard
+		/// discussion that took place into the database
+		/// using the persistence module.
 		/// </summary>
-		/// <param name="chats">
-		/// ChatContext array
+		/// <param name="chats">Array of ChatContext each of 
+		/// which would contain an array of chat messages
+		/// which would be used for the summarizer to 
+		/// save the summary in the database
+		/// </param>
+		/// <param name="fraction"> The fraction of the chat 
+		/// length which determines the length of summary
 		/// </param>
 		/// <returns>
-		/// Error code denoting the success or failure 
-		/// of the operation
+		/// Returns true if summary was succesfully stored 
+		/// and false otherwise
 		/// </returns>
 		public bool SaveSummary(ChatContext[] chats, double fraction = 0.2)
 		{
