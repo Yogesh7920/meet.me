35b29a02df1bc05dd341037519be47650b4c5a31
SahilChaudhari23 <91545445+SahilChaudhari23@users.noreply.github.com>
2021-11-08 10:21:34 +0530

    Create ContentModuleTest.cs

---
+++ Testing/Content/ContentModuleTest.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using Content;
+using System;
+using System.IO;
+using Networking;
+using MongoDB.Bson;
+
+namespace Testing
+{
+
+    public class ContentModuleTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
