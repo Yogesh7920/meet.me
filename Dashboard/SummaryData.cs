5a4388841dfeaacceee0641592ae92423e0a6913
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2021-11-09 01:24:35 +0530

    Added an Object for summary data

---
+++ Dashboard/SummaryData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard
+{
+    public class SummaryData : IRecievedFromServer
+    {
+        public SummaryData(string chatSummary)
+        {
+            summary = chatSummary;
+        }
+        public string summary;
+    }
+}
