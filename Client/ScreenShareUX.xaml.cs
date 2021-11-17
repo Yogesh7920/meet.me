bd3a5be3b915fd48ed92c5fd2c09987ae66fe65c
pulavartivinay <pulavartivinay@gmail.com>
2021-11-17 16:02:01 +0530

    created ScreenShareUX xaml file

---
+++ Client/ScreenShareUX.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ScreenSharing;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for ScreenShareUX.xaml
+    /// </summary>
+    public partial class ScreenShareUX : Window
+    {
+        public ScreenShareUX()
+        {
+            InitializeComponent();
+
+            ScreenShare Screenviewmodel = new ScreenShare();
+            this.DataContext = Screenviewmodel;
+
+            Console.WriteLine(Screenviewmodel.userid);
+        }
+    }
+}
---
+++ ScreenSharing/Client/ScreenShare.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * Owned By: Pulavarti Vinay Kumar
+ * Created By: Pulavarti Vinay Kumar
+ * Date Created: 16 Nov 2021
+ * Date Modified: 17 Nov 2021
+**/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using ScreenSharing;
+
+namespace ScreenSharing
+{
+    public class ScreenShare : IScreenShare
+    {
+        public string userid;
+        public string username;
+        public int _mtype;
+        public Bitmap _screen;
+        public void OnScreenRecieved(string uid, string uname, int mtype, Bitmap screen)
+        {
+            userid = "456";
+            username = "vinay";
+            _mtype = mtype;
+            _screen = screen;
+
+
+            // Problem in the network connection
+            //if (mtype == -2)
+            //{
+            //    Console.WriteLine("You have lost ur network connection");
+            //}
+            //else if (mtype == -1) // some one else is sharing so u can't share ur screen
+            //{
+            //    Console.WriteLine("some one else is sharing so u can't share ur screen");
+            //}
+            //else if (mtype == 0)  // Stop the screen share
+            //{
+
+            //}
+            //else if (mtype == 1)  // screen share is going now
+            //{
+
+            //}
+
+        }
+    }
+}
