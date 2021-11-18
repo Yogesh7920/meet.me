a2514be52057c5d038672a843db7854653c9bf33
ecstasy-repos <parmanandedx1234@gmail.com>
2021-11-18 12:47:37 +0530

    implemented the saveSummary function

--- Dashboard/Server/Persistence/SummaryPersistence.cs
+++ Dashboard/Server/Persistence/SummaryPersistence.cs
@@ -1,23 +1,31 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace Dashboard.Server.Persistence
 {
-   
+
     public class SummaryPersistence : ISummaryPersistence
     {
-        /// <summary>
-        /// saves the summary of the session into a summary file
-        /// </summary>
-        /// <param name="sessionId"> takes sessionId to denote the name of the file to which we need to save. </param>
-        /// <param name="message"> takes message string that need to be saved </param>
-        /// <returns> return true if succesfully saved else return false </returns>
-        public override bool saveSummary(String meetingId, string message)
+        public bool saveSummary(string message)
         {
-
+            string sessionId1 = string.Format("Summary_{0:yyyy - MM - dd_hh - mm - ss - tt}", DateTime.Now);
+            string path = "../../../Persistence/PersistenceDownloads/SummaryDownloads/";
+            string createText = "Summary : --------- " + Environment.NewLine + message + Environment.NewLine;
+            try
+            {
+                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+                File.WriteAllText(Path.Combine(path, sessionId1 + ".txt"), createText);
+                Trace.WriteLine("Summary saved Suceessfully!!");
+                return true;
+            }
+            catch (IOException exp)
+            {
+                Trace.WriteLine(exp.Message);
+                return false;
+            }
         }
     }
+
+
 }
