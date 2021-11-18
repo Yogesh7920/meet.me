6c21d451a80fbffa4723dc4f261516ebcd504a35
SahilChaudhari23 <91545445+SahilChaudhari23@users.noreply.github.com>
2021-11-18 11:07:43 +0530

    Method overloading with TestFlag boolean

--- Content/Client/FileClient.cs
+++ Content/Client/FileClient.cs
@@ -60,6 +60,43 @@ namespace Content
             _communicator.Send(toSendSerialized, "Content");
         }
 
+        public string Send(SendMessageData message, bool testFlag)
+        {
+            Trace.WriteLine("[FileClient] Received file send request");
+            if (message.Type != MessageType.File)
+                throw new ArgumentException("Message argument to FileClient::Send does not have MessageType File");
+
+            // check if file with given file path exists
+            var filepath = message.Message;
+
+            if (!File.Exists(filepath)) throw new FileNotFoundException("File {0} not found", filepath);
+
+            // initialize a MessageData object that will be sent to the server
+            var toSend = new MessageData();
+            var filedata = new SendFileData(filepath);
+
+            // set toSend's fields appropriately
+            toSend.Event = MessageEvent.NewMessage;
+            toSend.Type = MessageType.File;
+            toSend.MessageId = -1;
+            toSend.Message = filedata.fileName;
+            toSend.SenderId = UserId;
+            toSend.ReceiverIds = message.ReceiverIds;
+            toSend.ReplyThreadId = -1;
+            toSend.SentTime = DateTime.Now;
+            toSend.Starred = false;
+
+            toSend.FileData = filedata;
+
+            // serialize the message
+            Trace.WriteLine("[FileClient] Serializing the file data");
+            var toSendSerialized = _serializer.Serialize(toSend);
+
+            // send the message
+            Trace.WriteLine("[FileClient] Sending the file to server");
+            return toSendSerialized;
+        }
+
         public void Download(int messageId, string savepath)
         {
             var toSend = new MessageData();
@@ -75,6 +112,23 @@ namespace Content
 
             Trace.WriteLine("[FileClient] Sending file download request to server");
             _communicator.Send(toSendSerialized, "Content");
+        }
+
+        public string Download(int messageId, string savepath, bool testFlag)
+        {
+            var toSend = new MessageData();
+
+            toSend.Event = MessageEvent.Download;
+            toSend.Type = MessageType.File;
+            toSend.MessageId = messageId;
+            toSend.Message = savepath;
+            toSend.FileData = null;
+
+            // serialize the message and send via network
+            var toSendSerialized = _serializer.Serialize(toSend);
+
+            Trace.WriteLine("[FileClient] Sending file download request to server");
+            return toSendSerialized;
         }
     }
 }
