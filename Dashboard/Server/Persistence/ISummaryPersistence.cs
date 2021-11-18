436f1ad9960e4a8194abedbba5d6dfc20bf22cb7
ecstasy-repos <parmanandedx1234@gmail.com>
2021-11-18 13:58:22 +0530

    removed param session id

--- Dashboard/Server/Persistence/ISummaryPersistence.cs
+++ Dashboard/Server/Persistence/ISummaryPersistence.cs
@@ -12,7 +12,6 @@ namespace Dashboard.Server.Persistence
         /// <summary>
                  /// saves the summary of the session into a summary file
         /// </summary>
-        /// <param name="sessionId"> takes sessionId to denote the name of the file to which we need to save. </param>
         /// <param name="message"> takes message string that need to be saved </param>
         /// <returns> return true if succesfully saved else return false </returns>
         public bool saveSummary(string message);
