57bc171b1d26970c4a88092facdb3f8b6db439de
VM653 <111801055@smail.iitpkd.ac.in>
2021-11-19 13:22:02 +0530

    testTry1

--- Testing/Content/Client/ChatClientTest.cs
+++ Testing/Content/Client/ChatClientTest.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
-
+using Content;
+using FluentAssertions;
 namespace Testing.Content
 {
     public class ChatClientTests
@@ -10,9 +11,25 @@ namespace Testing.Content
         }
 
         [Test]
-        public void Test1()
+        public void TestConvert()
         {
-            Assert.Pass();
+			var toconvert = new SendMessageData();
+			toconvert.Message = "Apple";
+			toconvert.Type = MessageType.Chat;
+			toconvert.ReplyThreadId = -1;
+			toconvert.ReceiverIds = new int[0];
+            var x = SendToMessage(toconvert,MessageEvent.NewMessage);
+
+			Assert.AreEqual(x.Message, "Apple");
+			Assert.AreEqual(x.Event, MessageEvent.NewMessage);
+			Assert.AreEqual(x.Type, MessageType.Chat);
+			Assert.AreEqual(x.FileData, null);
+			Assert.That(typeof(x.SenderId).ToString() == "int")
+			Assert.AreEqual(x.Starred, false)
+			Assert.AreEqual(x.ReplyThreadId, -1);
+			Assert.AreEqual(x.ReceiverIds.Length, 0)
+
+
         }
     }
 }
