ac46712d04623fee3930ffa26704e814876c87fa
ecstasy-repos <parmanandedx1234@gmail.com>
2021-11-19 18:09:58 +0530

    updated according to naming conventions

---
+++ Dashboard/Server/Persistence/ITelemetryPersistence.cs
@@ -0,0 +1,33 @@
+//Author: Parmanand Kumar
+using Dashboard.Server.Telemetry;
+
+
+
+//Persistence
+namespace Dashboard.Server.Persistence
+{
+
+    public interface ITelemetryPersistence
+    {
+        /// <summary>
+        /// save the UserCountVsTimeStamp, UserIdVsChatCount, InsincereMember data as png after each session.
+        /// </summary>
+        /// <param name="intraSessionData"> takes IntraSessionData from Telemetry. </param>
+        public bool Save(SessionAnalytics sessionAnalyticsData);
+
+        /// <summary>
+        /// append the ServerData into a file after each session end
+        /// </summary>
+        /// <param name="AllserverData"> takes ServerData from Telemetry to be saved into text file </param> 
+        /// <returns>Returns true if saved successfully else returns false</returns>
+        public ResponseEntity SaveServerData(ServerDataToSave AllserverData);
+
+        /// <summary>
+        /// retrives the ServerData after end of all of the sessions.
+        /// </summary>
+        /// <returns>returns List of SeverData</returns>
+        public ServerDataToSave RetriveAllSeverData();
+    }
+
+
+}
