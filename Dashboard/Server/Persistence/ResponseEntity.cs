2824bc9f2f143017a5e33b5a291d1e934cff94da
ecstasy-repos <parmanandedx1234@gmail.com>
2021-11-19 18:12:10 +0530

    required for returning variable name of file after being saved

---
+++ Dashboard/Server/Persistence/ResponseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Dashboard.Server.Persistence
+{
+    // ResponseEntity 
+    public class ResponseEntity
+    {
+        // True if file saved successfully
+        public bool IsSaved;
+
+        // name of the file 
+        public string FileName;
+    }
+
+}
