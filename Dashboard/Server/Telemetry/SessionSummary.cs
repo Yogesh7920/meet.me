63da00264034ceb48e7f0c6d81188e83182cd88b
ecstasy-repos <parmanandedx1234@gmail.com>
2021-11-19 19:29:46 +0530

    Added sample code

---
+++ Dashboard/Server/Telemetry/SessionSummary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dashboard.Server.Telemetry
+{
+    public class SessionSummary
+    {
+        /// The starting time of the session
+        public DateTime SessionStartTime;
+
+        /// The  score of the session
+        public int Score;
+
+        /// Total User count in the session
+        public int UserCount;
+
+        /// Total chats in the session
+        public int ChatCount;
+    }
+}
