c13a62ddeb23bf655a869b9d189ca57ea0c0f64a
ecstasy-repos <parmanandedx1234@gmail.com>
2021-11-20 07:19:46 +0530

    Updated Factory Model To create its static instance and only once

--- Dashboard/Server/Persistence/PersistenceFactory.cs
+++ Dashboard/Server/Persistence/PersistenceFactory.cs
@@ -6,21 +6,38 @@ namespace Dashboard.Server.Persistence
     /// <summary>
     ///     create instances of Summamry or Telemetry Persistence Class respectively
     /// </summary>
-    public class PersistenceFactory
+    public static class PersistenceFactory
     {
+
+        static PersistenceFactory()
+        {
+            if(_summaryPersisitence == null)
+            {
+                _summaryPersisitence = new SummaryPersistence();
+            }
+
+            if(_telemetryPersisitence == null)
+            {
+                _telemetryPersisitence = new TelemetryPersistence();
+            }
+
+        }
         /// <summary>Helps to create instance of ISummaryPersistence </summary>
         /// <returns> return ITelemetryPersistence Interface which enables to use functions of class inheriting the same </returns>
-        public ISummaryPersistence GetSummaryPersistenceInstance()
+        public static ISummaryPersistence GetSummaryPersistenceInstance()
         {
             return new SummaryPersistence();
         }
 
         /// <summary>Helps to create instance of ITelemetryPersistence </summary>
         /// <returns> return ITelemetryPersistence Interface which enables to use functions of class inheriting the same </returns>
-        public ITelemetryPersistence GetTelemetryPersistenceInstance()
+        public static ITelemetryPersistence GetTelemetryPersistenceInstance()
         {
             return new TelemetryPersistence();
         }
+
+        private static ISummaryPersistence _summaryPersisitence;
+        private static ITelemetryPersistence _telemetryPersisitence;
 
     }
 
