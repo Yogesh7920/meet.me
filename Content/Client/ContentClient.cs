bc6bf3e785c44dcb14ae0a5d77bd8dcf14133fd0
VM653 <111801055@smail.iitpkd.ac.in>
2021-11-20 15:22:07 +0530

    Chat Update Update

--- Content/Client/ContentClient.cs
+++ Content/Client/ContentClient.cs
@@ -132,7 +132,7 @@ namespace Content
         /// <inheritdoc />
         public void CUpdateChat(int messageId, string newMessage)
         {
-            throw new NotImplementedException();
+            _chatHandler.ChatUpdate(int messageId, string newMessage);
         }
 
         /// <inheritdoc />
