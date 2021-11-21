09c6518b1465839817927315e0227712bb2bfac1
bruise_88 <sidshah1036.ss@gmail.com>
2021-11-21 13:08:29 +0530

    updated namespaces for testing modules

--- Testing/Dashboard/Persistence.UnitTests/PersistenceNUnitTests.cs
+++ Testing/Dashboard/Persistence.UnitTests/PersistenceNUnitTests.cs
@@ -14,7 +14,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
-namespace Testing.Dashboard
+namespace Testing.Dashboard.Persistence
 {
     public class PersistenceTests
     {
