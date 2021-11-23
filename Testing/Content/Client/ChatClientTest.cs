be647fb15fcf9b432f7ac3920ee20aaa7d2a3ca0
VM653 <111801055@smail.iitpkd.ac.in>
2021-11-23 10:29:21 +0530

    Test Send New message

--- Testing/Content/Client/ChatClientTest.cs
+++ Testing/Content/Client/ChatClientTest.cs
@@ -1,5 +1,8 @@
 using Content;
+using Networking;
 using NUnit.Framework;
+using System;
+using System.IO;
 namespace Testing.Content
 {
     public class ChatClientTests
@@ -12,7 +15,7 @@ namespace Testing.Content
         }
 
         [Test]
-        public void TestConvert()
+        public void Test_Converting_SendMessageObject_To_MessageData()
         {
             Utils _util = new Utils();
             SendMessageData SampleData = _util.GetSendMessageData2();
@@ -27,5 +30,43 @@ namespace Testing.Content
             Assert.AreEqual(x.ReplyThreadId, SampleData.ReplyThreadId);
             Assert.AreEqual(x.ReceiverIds.Length, SampleData.ReceiverIds.Length);
         }
+		[Test]
+		public void Test_ChatNewMessage()
+		{
+			Utils _util = new Utils();
+            int UserId = 1001;
+            SendMessageData SampleData = _util.GenerateChatSendMsgData("Apple", new int[] { 1002 }, type: MessageType.Chat);
+            MessageData SampleMsgData = _util.GenerateChatMessageData(MessageEvent.NewMessage, "Apple", new int[] { 1002 }, type: MessageType.Chat);
+
+            ISerializer _serializer = new Serializer();
+           
+			ChatClient _contentChat = new ChatClient(_util.GetFakeCommunicator());
+            FakeCommunicator _fakeCommunicator = _util.GetFakeCommunicator();
+            _contentChat.UserId = UserId;
+            _contentChat.Communicator = _fakeCommunicator;
+
+            _contentChat.ChatNewMessage(SampleData);
+
+            var sendSerializedMsg = _fakeCommunicator.GetSentData();
+            var deserialized = _serializer.Deserialize<MessageData>(sendSerializedMsg);
+
+            if (deserialized is MessageData)
+            {
+                var receivedMessage = deserialized as MessageData;
+                Assert.AreEqual(receivedMessage.Message, SampleMsgData.Message);
+                Assert.AreEqual(receivedMessage.Event, MessageEvent.NewMessage);
+                Assert.AreEqual(receivedMessage.Type, SampleMsgData.Type);
+                Assert.AreEqual(receivedMessage.FileData, SampleMsgData.FileData);
+                Assert.AreEqual(receivedMessage.Starred, SampleMsgData.Starred);
+                Assert.AreEqual(receivedMessage.ReplyThreadId, SampleMsgData.ReplyThreadId);
+                Assert.AreEqual(receivedMessage.SenderId, UserId);
+                Assert.AreEqual(receivedMessage.ReceiverIds.Length, SampleMsgData.ReceiverIds.Length);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+		}
     }
 }
