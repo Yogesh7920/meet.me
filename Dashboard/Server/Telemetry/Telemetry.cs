c60850dd9a81f2b2f406f7b18665684488df4771
h-a-r-s-h-p <111801015@smail.iitpkd.ac.in>
2021-11-23 22:44:17 +0530

    configured names in tests

--- Dashboard/Server/Telemetry/Telemetry.cs
+++ Dashboard/Server/Telemetry/Telemetry.cs
@@ -80,7 +80,7 @@ namespace Dashboard.Server.Telemetry{
             {
                 UserData  currUser = user_i.Key;
                 // if difference of exit and enter time is less than 30 min.
-                if(userExitTime.ContainsKey(currUser) && userExitTime[currUser].Subtract(user_i.Value).TotalMinutes<30)
+                if(userExitTime.ContainsKey(currUser) && userExitTime[currUser].Subtract(user_i.Value).TotalMinutes < thresholdTime)
                 {
                     int id = currUser.userID;
                     insincereMembers.Add(id);
@@ -165,6 +165,7 @@ namespace Dashboard.Server.Telemetry{
         public Dictionary<int, int> userIdChatCountDic= new Dictionary<int, int>();
         public List<int> insincereMembers= new List<int>();
         private readonly ITelemetryPersistence _persistence = PersistenceFactory.GetTelemetryPersistenceInstance();
+        private int thresholdTime = 30;
         // private ITelemetrySessionManager _sm = new ITelemetrySessionManager();
     }
 }
