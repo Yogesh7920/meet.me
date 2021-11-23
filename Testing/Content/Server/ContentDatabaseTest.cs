e2cc0c2764e95b3000037da6bd181c4147896e61
sameer38 <111801038@smail.iitpkd.ac.in>
2021-11-23 18:57:27 +0530

    File and Message Tests

--- Testing/Content/Server/ContentDatabaseTest.cs
+++ Testing/Content/Server/ContentDatabaseTest.cs
@@ -2,130 +2,131 @@ using NUnit.Framework;
 using Content;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Testing.Content
 {
     public class ContentDatabaseTests
     {
         private ContentDatabase contentDatabase;
+        private Utils _utils;
 
         [SetUp]
         public void Setup()
         {
             contentDatabase = new ContentDatabase();
+            _utils = new Utils();
         }
 
         [Test]
         public void FileTest()
         {
-            //string pathA = @"trace.txt";
-            //string pathB = @"whiteboard.dll";
+            string CurrentDirectory = Directory.GetCurrentDirectory() as string;
+            string[] path = CurrentDirectory.Split(new string[] { "\\Testing" }, StringSplitOptions.None);
+            string pathA = path[0] + "\\Testing\\Content\\Test_File.pdf";
 
-            //MessageData file1 = new MessageData();
-            //file1.Message = "trace.txt";
-            //file1.Type = MessageType.File;
-            //file1.FileData = new SendFileData(pathA);
-            //file1.SenderId = 1;
-            //file1.ReplyThreadId = -1;
-            //file1.Event = MessageEvent.NewMessage;
+            string pathB = path[0] + "\\Testing\\Content\\Test_File.pdf";
 
-            //MessageData recv = contentDatabase.StoreFile(file1);
+            MessageData file1 = new MessageData();
+            file1.Message = "trace.txt";
+            file1.Type = MessageType.File;
+            file1.FileData = new SendFileData(pathA);
+            file1.SenderId = 1;
+            file1.ReplyThreadId = -1;
+            file1.Event = MessageEvent.NewMessage;
 
-            //Assert.AreEqual(file1.Message, recv.Message);
-            //Assert.AreEqual(file1.Type, recv.Type);
-            //Assert.AreEqual(file1.SenderId, recv.SenderId);
-            //Assert.AreEqual(file1.Event, recv.Event);
-            //Assert.AreEqual(file1.FileData.fileSize, recv.FileData.fileSize);
-            //Assert.AreEqual(file1.FileData.fileName, recv.FileData.fileName);
-            //Assert.AreEqual(file1.FileData.fileContent, recv.FileData.fileContent);
+            MessageData recv = contentDatabase.StoreFile(file1);
 
-            //MessageData file2 = new MessageData();
-            //file2.Message = "whiteboard.dll";
-            //file2.Type = MessageType.File;
-            //file2.FileData = new SendFileData(pathB);
-            //file2.SenderId = 1;
-            //file2.ReplyThreadId = -1;
-            //file2.Event = MessageEvent.NewMessage;
+            Assert.AreEqual(file1.Message, recv.Message);
+            Assert.AreEqual(file1.Type, recv.Type);
+            Assert.AreEqual(file1.SenderId, recv.SenderId);
+            Assert.AreEqual(file1.Event, recv.Event);
+            Assert.AreEqual(file1.FileData.fileSize, recv.FileData.fileSize);
+            Assert.AreEqual(file1.FileData.fileName, recv.FileData.fileName);
+            Assert.AreEqual(file1.FileData.fileContent, recv.FileData.fileContent);
 
-            //recv = contentDatabase.StoreFile(file2);
+            MessageData file2 = new MessageData();
+            file2.Message = "whiteboard.dll";
+            file2.Type = MessageType.File;
+            file2.FileData = new SendFileData(pathB);
+            file2.SenderId = 1;
+            file2.ReplyThreadId = -1;
+            file2.Event = MessageEvent.NewMessage;
 
-            //Assert.AreEqual(file2.Message, recv.Message);
-            //Assert.AreNotEqual(recv.MessageId, file1.MessageId);
-            //Assert.AreNotEqual(recv.ReplyThreadId, file1.ReplyThreadId);
-            //Assert.AreEqual(file2.Type, recv.Type);
-            //Assert.AreEqual(file2.SenderId, recv.SenderId);
-            //Assert.AreEqual(file2.Event, recv.Event);
-            //Assert.AreEqual(file2.FileData.fileSize, recv.FileData.fileSize);
-            //Assert.AreEqual(file2.FileData.fileName, recv.FileData.fileName);
-            //Assert.AreEqual(file2.FileData.fileContent, recv.FileData.fileContent);
+            recv = contentDatabase.StoreFile(file2);
 
-            //MessageData file3 = new MessageData();
-            //file3.Message = "c.txt";
-            //file3.Type = MessageType.File;
-            //file3.FileData = new SendFileData(pathB);
-            //file3.SenderId = 1;
-            //file3.ReplyThreadId = file1.ReplyThreadId;
-            //file3.Event = MessageEvent.NewMessage;
+            Assert.AreEqual(file2.Message, recv.Message);
+            Assert.AreNotEqual(recv.MessageId, file1.MessageId);
+            Assert.AreNotEqual(recv.ReplyThreadId, file1.ReplyThreadId);
+            Assert.AreEqual(file2.Type, recv.Type);
+            Assert.AreEqual(file2.SenderId, recv.SenderId);
+            Assert.AreEqual(file2.Event, recv.Event);
+            Assert.AreEqual(file2.FileData.fileSize, recv.FileData.fileSize);
+            Assert.AreEqual(file2.FileData.fileName, recv.FileData.fileName);
+            Assert.AreEqual(file2.FileData.fileContent, recv.FileData.fileContent);
 
-            //recv = contentDatabase.StoreFile(file3);
+            MessageData file3 = new MessageData();
+            file3.Message = "c.txt";
+            file3.Type = MessageType.File;
+            file3.FileData = new SendFileData(pathB);
+            file3.SenderId = 1;
+            file3.ReplyThreadId = file1.ReplyThreadId;
+            file3.Event = MessageEvent.NewMessage;
 
-            //Assert.AreEqual(file3.Message, recv.Message);
-            //Assert.AreNotEqual(recv.MessageId, file1.MessageId);
-            //Assert.AreNotEqual(recv.MessageId, file2.MessageId);
-            //Assert.AreEqual(recv.ReplyThreadId, file1.ReplyThreadId);
-            //Assert.AreEqual(file3.Type, recv.Type);
-            //Assert.AreEqual(file3.SenderId, recv.SenderId);
-            //Assert.AreEqual(file3.Event, recv.Event);
-            //Assert.AreEqual(file3.FileData.fileSize, recv.FileData.fileSize);
-            //Assert.AreEqual(file3.FileData.fileName, recv.FileData.fileName);
-            //Assert.AreEqual(file3.FileData.fileContent, recv.FileData.fileContent);
+            recv = contentDatabase.StoreFile(file3);
 
-            //recv = contentDatabase.GetFiles(file1.MessageId);
+            Assert.AreEqual(file3.Message, recv.Message);
+            Assert.AreNotEqual(recv.MessageId, file1.MessageId);
+            Assert.AreNotEqual(recv.MessageId, file2.MessageId);
+            Assert.AreEqual(recv.ReplyThreadId, file1.ReplyThreadId);
+            Assert.AreEqual(file3.Type, recv.Type);
+            Assert.AreEqual(file3.SenderId, recv.SenderId);
+            Assert.AreEqual(file3.Event, recv.Event);
+            Assert.AreEqual(file3.FileData.fileSize, recv.FileData.fileSize);
+            Assert.AreEqual(file3.FileData.fileName, recv.FileData.fileName);
+            Assert.AreEqual(file3.FileData.fileContent, recv.FileData.fileContent);
 
-            //Assert.AreEqual(file1.Message, recv.Message);
-            //Assert.AreEqual(file1.MessageId, recv.MessageId);
-            //Assert.AreEqual(file1.Type, recv.Type);
-            //Assert.AreEqual(file1.SenderId, recv.SenderId);
-            //Assert.AreEqual(file1.Event, recv.Event);
-            //Assert.AreEqual(file1.FileData.fileSize, recv.FileData.fileSize);
-            //Assert.AreEqual(file1.FileData.fileName, recv.FileData.fileName);
-            //Assert.AreEqual(file1.FileData.fileContent, recv.FileData.fileContent);
-            //Assert.AreEqual(file1.ReplyThreadId, recv.ReplyThreadId);
+            recv = contentDatabase.GetFiles(file1.MessageId);
 
-            //recv = contentDatabase.GetFiles(file2.MessageId);
+            Assert.AreEqual(file1.Message, recv.Message);
+            Assert.AreEqual(file1.MessageId, recv.MessageId);
+            Assert.AreEqual(file1.Type, recv.Type);
+            Assert.AreEqual(file1.SenderId, recv.SenderId);
+            Assert.AreEqual(file1.Event, recv.Event);
+            Assert.AreEqual(file1.FileData.fileSize, recv.FileData.fileSize);
+            Assert.AreEqual(file1.FileData.fileName, recv.FileData.fileName);
+            Assert.AreEqual(file1.FileData.fileContent, recv.FileData.fileContent);
+            Assert.AreEqual(file1.ReplyThreadId, recv.ReplyThreadId);
 
-            //Assert.AreEqual(file2.Message, recv.Message);
-            //Assert.AreEqual(file2.MessageId, recv.MessageId);
-            //Assert.AreEqual(file2.Type, recv.Type);
-            //Assert.AreEqual(file2.SenderId, recv.SenderId);
-            //Assert.AreEqual(file2.Event, recv.Event);
-            //Assert.AreEqual(file2.FileData.fileSize, recv.FileData.fileSize);
-            //Assert.AreEqual(file2.FileData.fileName, recv.FileData.fileName);
-            //Assert.AreEqual(file2.FileData.fileContent, recv.FileData.fileContent);
-            //Assert.AreEqual(file2.ReplyThreadId, recv.ReplyThreadId);
+            recv = contentDatabase.GetFiles(file2.MessageId);
 
-            //recv = contentDatabase.GetFiles(file3.MessageId);
+            Assert.AreEqual(file2.Message, recv.Message);
+            Assert.AreEqual(file2.MessageId, recv.MessageId);
+            Assert.AreEqual(file2.Type, recv.Type);
+            Assert.AreEqual(file2.SenderId, recv.SenderId);
+            Assert.AreEqual(file2.Event, recv.Event);
+            Assert.AreEqual(file2.FileData.fileSize, recv.FileData.fileSize);
+            Assert.AreEqual(file2.FileData.fileName, recv.FileData.fileName);
+            Assert.AreEqual(file2.FileData.fileContent, recv.FileData.fileContent);
+            Assert.AreEqual(file2.ReplyThreadId, recv.ReplyThreadId);
 
-            //Assert.AreEqual(file3.Message, recv.Message);
-            //Assert.AreEqual(file3.MessageId, recv.MessageId);
-            //Assert.AreEqual(file3.Type, recv.Type);
-            //Assert.AreEqual(file3.SenderId, recv.SenderId);
-            //Assert.AreEqual(file3.Event, recv.Event);
-            //Assert.AreEqual(file3.FileData.fileSize, recv.FileData.fileSize);
-            //Assert.AreEqual(file3.FileData.fileName, recv.FileData.fileName);
-            //Assert.AreEqual(file3.FileData.fileContent, recv.FileData.fileContent);
-            //Assert.AreEqual(file3.ReplyThreadId, recv.ReplyThreadId);
+            recv = contentDatabase.GetFiles(file3.MessageId);
+
+            Assert.AreEqual(file3.Message, recv.Message);
+            Assert.AreEqual(file3.MessageId, recv.MessageId);
+            Assert.AreEqual(file3.Type, recv.Type);
+            Assert.AreEqual(file3.SenderId, recv.SenderId);
+            Assert.AreEqual(file3.Event, recv.Event);
+            Assert.AreEqual(file3.FileData.fileSize, recv.FileData.fileSize);
+            Assert.AreEqual(file3.FileData.fileName, recv.FileData.fileName);
+            Assert.AreEqual(file3.FileData.fileContent, recv.FileData.fileContent);
+            Assert.AreEqual(file3.ReplyThreadId, recv.ReplyThreadId);
         }
 
         [Test]
         public void MessageTest()
         {
-            MessageData message1 = new MessageData();
-            message1.Message = "Hello";
-            message1.Type = MessageType.Chat;
-            message1.SenderId = 1;
-            message1.ReplyThreadId = 0;
-            message1.Event = MessageEvent.NewMessage;
+            MessageData message1 = _utils.GenerateNewMessageData("Hello", SenderId: 1);
 
             MessageData recv = contentDatabase.StoreMessage(message1);
 
@@ -135,12 +136,7 @@ namespace Testing.Content
             Assert.AreEqual(message1.Event, recv.Event);
             Assert.IsNull(recv.FileData);
 
-            MessageData message2 = new MessageData();
-            message2.Message = "Hello2";
-            message2.Type = MessageType.Chat;
-            message2.SenderId = 1;
-            message2.ReplyThreadId = message1.ReplyThreadId;
-            message2.Event = MessageEvent.NewMessage;
+            MessageData message2 = _utils.GenerateNewMessageData("Hello2", SenderId: 1, ReplyThreadId: message1.ReplyThreadId);
 
             recv = contentDatabase.StoreMessage(message2);
 
@@ -152,12 +148,7 @@ namespace Testing.Content
             Assert.IsNull(recv.FileData);
             Assert.AreEqual(message2.ReplyThreadId, recv.ReplyThreadId);
 
-            MessageData message3 = new MessageData();
-            message3.Message = "Hello3";
-            message3.Type = MessageType.Chat;
-            message3.SenderId = 1;
-            message3.ReplyThreadId = -1;
-            message3.Event = MessageEvent.NewMessage;
+            MessageData message3 = _utils.GenerateNewMessageData("Hello3", SenderId: 1);
 
             recv = contentDatabase.StoreMessage(message3);
 
--- Testing/Content/Server/FileServerTest.cs
+++ Testing/Content/Server/FileServerTest.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using Content;
+using System.IO;
+using System;
 
 namespace Testing.Content
 {
@@ -15,103 +17,106 @@ namespace Testing.Content
             fileServer = new FileServer(database);
         }
 
-        //[Test]
-        //public void FileStoreAndFetchTest()
-        //{
-        //    string pathA = @"trace.txt";
-        //    string pathB = @"whiteboard.dll";
+        [Test]
+        public void FileStoreAndFetchTest()
+        {
+            string CurrentDirectory = Directory.GetCurrentDirectory() as string;
+            string[] path = CurrentDirectory.Split(new string[] { "\\Testing" }, StringSplitOptions.None);
+            string pathA = path[0] + "\\Testing\\Content\\Test_File.pdf";
 
-        //    MessageData file1 = new MessageData();
-        //    file1.Message = "trace.txt";
-        //    file1.Type = MessageType.File;
-        //    file1.FileData = new SendFileData(pathA);
-        //    file1.SenderId = 1;
-        //    file1.ReplyThreadId = -1;
-        //    file1.Event = MessageEvent.NewMessage;
+            string pathB = path[0] + "\\Testing\\Content\\Test_File.pdf";
 
-        //    MessageData recv = fileServer.Receive(file1);
+            MessageData file1 = new MessageData();
+            file1.Message = "trace.txt";
+            file1.Type = MessageType.File;
+            file1.FileData = new SendFileData(pathA);
+            file1.SenderId = 1;
+            file1.ReplyThreadId = -1;
+            file1.Event = MessageEvent.NewMessage;
 
-        //    Assert.AreEqual(file1.Message, recv.Message);
-        //    Assert.AreEqual(file1.Type, recv.Type);
-        //    Assert.AreEqual(file1.SenderId, recv.SenderId);
-        //    Assert.AreEqual(file1.Event, recv.Event);
-        //    Assert.IsNull(recv.FileData);
+            MessageData recv = fileServer.Receive(file1);
 
-        //    MessageData file2 = new MessageData();
-        //    file2.Message = "whiteboard.dll";
-        //    file2.Type = MessageType.File;
-        //    file2.FileData = new SendFileData(pathB);
-        //    file2.SenderId = 1;
-        //    file2.ReplyThreadId = -1;
-        //    file2.Event = MessageEvent.NewMessage;
+            Assert.AreEqual(file1.Message, recv.Message);
+            Assert.AreEqual(file1.Type, recv.Type);
+            Assert.AreEqual(file1.SenderId, recv.SenderId);
+            Assert.AreEqual(file1.Event, recv.Event);
+            Assert.IsNull(recv.FileData);
 
-        //    recv = fileServer.Receive(file2);
+            MessageData file2 = new MessageData();
+            file2.Message = "whiteboard.dll";
+            file2.Type = MessageType.File;
+            file2.FileData = new SendFileData(pathB);
+            file2.SenderId = 1;
+            file2.ReplyThreadId = -1;
+            file2.Event = MessageEvent.NewMessage;
 
-        //    Assert.AreEqual(file2.Message, recv.Message);
-        //    Assert.AreNotEqual(recv.MessageId, file1.MessageId);
-        //    Assert.AreNotEqual(recv.ReplyThreadId, file1.ReplyThreadId);
-        //    Assert.AreEqual(file2.Type, recv.Type);
-        //    Assert.AreEqual(file2.SenderId, recv.SenderId);
-        //    Assert.AreEqual(file2.Event, recv.Event);
-        //    Assert.IsNull(recv.FileData);
+            recv = fileServer.Receive(file2);
 
-        //    MessageData file3 = new MessageData();
-        //    file3.Message = "c.txt";
-        //    file3.Type = MessageType.File;
-        //    file3.FileData = new SendFileData(pathB);
-        //    file3.SenderId = 1;
-        //    file3.ReplyThreadId = file1.ReplyThreadId;
-        //    file3.Event = MessageEvent.NewMessage;
+            Assert.AreEqual(file2.Message, recv.Message);
+            Assert.AreNotEqual(recv.MessageId, file1.MessageId);
+            Assert.AreNotEqual(recv.ReplyThreadId, file1.ReplyThreadId);
+            Assert.AreEqual(file2.Type, recv.Type);
+            Assert.AreEqual(file2.SenderId, recv.SenderId);
+            Assert.AreEqual(file2.Event, recv.Event);
+            Assert.IsNull(recv.FileData);
 
-        //    recv = fileServer.Receive(file3);
+            MessageData file3 = new MessageData();
+            file3.Message = "c.txt";
+            file3.Type = MessageType.File;
+            file3.FileData = new SendFileData(pathB);
+            file3.SenderId = 1;
+            file3.ReplyThreadId = file1.ReplyThreadId;
+            file3.Event = MessageEvent.NewMessage;
 
-        //    Assert.AreEqual(file3.Message, recv.Message);
-        //    Assert.AreNotEqual(recv.MessageId, file1.MessageId);
-        //    Assert.AreNotEqual(recv.MessageId, file2.MessageId);
-        //    Assert.AreEqual(recv.ReplyThreadId, file1.ReplyThreadId);
-        //    Assert.AreEqual(file3.Type, recv.Type);
-        //    Assert.AreEqual(file3.SenderId, recv.SenderId);
-        //    Assert.AreEqual(file3.Event, recv.Event);
-        //    Assert.IsNull(recv.FileData);
+            recv = fileServer.Receive(file3);
 
-        //    file1.Event = MessageEvent.Download;
-        //    recv = fileServer.Receive(file1);
+            Assert.AreEqual(file3.Message, recv.Message);
+            Assert.AreNotEqual(recv.MessageId, file1.MessageId);
+            Assert.AreNotEqual(recv.MessageId, file2.MessageId);
+            Assert.AreEqual(recv.ReplyThreadId, file1.ReplyThreadId);
+            Assert.AreEqual(file3.Type, recv.Type);
+            Assert.AreEqual(file3.SenderId, recv.SenderId);
+            Assert.AreEqual(file3.Event, recv.Event);
+            Assert.IsNull(recv.FileData);
 
-        //    Assert.AreEqual(file1.Message, recv.Message);
-        //    Assert.AreEqual(file1.MessageId, recv.MessageId);
-        //    Assert.AreEqual(file1.Type, recv.Type);
-        //    Assert.AreEqual(file1.SenderId, recv.SenderId);
-        //    Assert.AreEqual(file1.Event, recv.Event);
-        //    Assert.AreEqual(file1.FileData.fileSize, recv.FileData.fileSize);
-        //    Assert.AreEqual(file1.FileData.fileName, recv.FileData.fileName);
-        //    Assert.AreEqual(file1.FileData.fileContent, recv.FileData.fileContent);
-        //    Assert.AreEqual(file1.ReplyThreadId, recv.ReplyThreadId);
+            file1.Event = MessageEvent.Download;
+            recv = fileServer.Receive(file1);
 
-        //    file2.Event = MessageEvent.Download;
-        //    recv = fileServer.Receive(file2);
+            Assert.AreEqual(file1.Message, recv.Message);
+            Assert.AreEqual(file1.MessageId, recv.MessageId);
+            Assert.AreEqual(file1.Type, recv.Type);
+            Assert.AreEqual(file1.SenderId, recv.SenderId);
+            Assert.AreEqual(file1.Event, recv.Event);
+            Assert.AreEqual(file1.FileData.fileSize, recv.FileData.fileSize);
+            Assert.AreEqual(file1.FileData.fileName, recv.FileData.fileName);
+            Assert.AreEqual(file1.FileData.fileContent, recv.FileData.fileContent);
+            Assert.AreEqual(file1.ReplyThreadId, recv.ReplyThreadId);
 
-        //    Assert.AreEqual(file2.Message, recv.Message);
-        //    Assert.AreEqual(file2.MessageId, recv.MessageId);
-        //    Assert.AreEqual(file2.Type, recv.Type);
-        //    Assert.AreEqual(file2.SenderId, recv.SenderId);
-        //    Assert.AreEqual(file2.Event, recv.Event);
-        //    Assert.AreEqual(file2.FileData.fileSize, recv.FileData.fileSize);
-        //    Assert.AreEqual(file2.FileData.fileName, recv.FileData.fileName);
-        //    Assert.AreEqual(file2.FileData.fileContent, recv.FileData.fileContent);
-        //    Assert.AreEqual(file2.ReplyThreadId, recv.ReplyThreadId);
+            file2.Event = MessageEvent.Download;
+            recv = fileServer.Receive(file2);
 
-        //    file3.Event = MessageEvent.Download;
-        //    recv = fileServer.Receive(file3);
+            Assert.AreEqual(file2.Message, recv.Message);
+            Assert.AreEqual(file2.MessageId, recv.MessageId);
+            Assert.AreEqual(file2.Type, recv.Type);
+            Assert.AreEqual(file2.SenderId, recv.SenderId);
+            Assert.AreEqual(file2.Event, recv.Event);
+            Assert.AreEqual(file2.FileData.fileSize, recv.FileData.fileSize);
+            Assert.AreEqual(file2.FileData.fileName, recv.FileData.fileName);
+            Assert.AreEqual(file2.FileData.fileContent, recv.FileData.fileContent);
+            Assert.AreEqual(file2.ReplyThreadId, recv.ReplyThreadId);
 
-        //    Assert.AreEqual(file3.Message, recv.Message);
-        //    Assert.AreEqual(file3.MessageId, recv.MessageId);
-        //    Assert.AreEqual(file3.Type, recv.Type);
-        //    Assert.AreEqual(file3.SenderId, recv.SenderId);
-        //    Assert.AreEqual(file3.Event, recv.Event);
-        //    Assert.AreEqual(file3.FileData.fileSize, recv.FileData.fileSize);
-        //    Assert.AreEqual(file3.FileData.fileName, recv.FileData.fileName);
-        //    Assert.AreEqual(file3.FileData.fileContent, recv.FileData.fileContent);
-        //    Assert.AreEqual(file3.ReplyThreadId, recv.ReplyThreadId);
-        //}
+            file3.Event = MessageEvent.Download;
+            recv = fileServer.Receive(file3);
+
+            Assert.AreEqual(file3.Message, recv.Message);
+            Assert.AreEqual(file3.MessageId, recv.MessageId);
+            Assert.AreEqual(file3.Type, recv.Type);
+            Assert.AreEqual(file3.SenderId, recv.SenderId);
+            Assert.AreEqual(file3.Event, recv.Event);
+            Assert.AreEqual(file3.FileData.fileSize, recv.FileData.fileSize);
+            Assert.AreEqual(file3.FileData.fileName, recv.FileData.fileName);
+            Assert.AreEqual(file3.FileData.fileContent, recv.FileData.fileContent);
+            Assert.AreEqual(file3.ReplyThreadId, recv.ReplyThreadId);
+        }
     }
 }
