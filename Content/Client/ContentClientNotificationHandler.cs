ad38d6df01120ac469623544ae17637570ecced5
SahilChaudhari23 <91545445+SahilChaudhari23@users.noreply.github.com>
2021-11-25 18:59:30 +0530

    Update ContentClientNotificationHandler.cs

--- Content/Client/ContentClientNotificationHandler.cs
+++ Content/Client/ContentClientNotificationHandler.cs
@@ -40,7 +40,7 @@ namespace Content
                 _contentHandler.OnReceive(_receivedMessage);
             }
 
-            else if (string.Equals(deserializedType, "Content.ArrayOfChatContext"))
+            else if (string.Equals(deserializedType, typeof(List<ChatContext>).ToString()))
             {
                 _allMessages = _serializer.Deserialize<List<ChatContext>>(data);
                 _contentHandler.OnReceive(_allMessages);
