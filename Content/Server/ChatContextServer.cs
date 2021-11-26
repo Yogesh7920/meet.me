4ca0cfe03ac2117c7a7ebbd80c4a408c8e7bd2da
Yuvraj Raghuvanshi <111801048@smail.iitpkd.ac.in>
2021-11-26 17:56:29 +0530

    Bug fix: The event field in the database shouldn't change when starring or updating a message.

--- Content/Server/ChatContextServer.cs
+++ Content/Server/ChatContextServer.cs
@@ -57,8 +57,10 @@ namespace Content
                 return null;
             }
 
-            // Else create a MessageData object from ReceiveMessageData and return that
-            return new MessageData(receiveMessageData);
+            // Else create a MessageData object from ReceiveMessageData and return that to be notified to clients
+            MessageData notifyMessageData = new MessageData(receiveMessageData);
+            notifyMessageData.Event = messageData.Event;
+            return notifyMessageData;
         }
 
         /// <summary>
@@ -88,7 +90,6 @@ namespace Content
 
             // Star the message and return the starred message
             message.Starred = !message.Starred;
-            message.Event = MessageEvent.Star;
             return message;
         }
 
@@ -110,7 +111,6 @@ namespace Content
 
             // Update the message and return the updated message
             message.Message = msgString;
-            message.Event = MessageEvent.Update;
             return message;
         }
     }
