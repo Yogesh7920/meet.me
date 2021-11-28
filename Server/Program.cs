bf792bcfff22b9ba73caad33b9abd82add7e9361
pulavartivinay <pulavartivinay@gmail.com>
2021-11-28 23:17:36 +0530

    followed name conventionsfor the variables

--- Server/Program.cs
+++ Server/Program.cs
@@ -7,6 +7,7 @@
 
 using System;
 using Dashboard;
+using Dashboard.Server.SessionManagement;
 
 namespace Server
 {
@@ -14,12 +15,12 @@ namespace Server
     {
         private static void Main(string[] args)
         {
-            var ServerSM = SessionManagerFactory.GetServerSessionManager();
+            ServerSessionManager serverSM = SessionManagerFactory.GetServerSessionManager();
 
             // if the input command entered is startMeet, will call the GetPortsAndIPAddress function which creates the meeting and 
             // returns the object which contains IPAddress and Port of the meeting
-            var Meeting = ServerSM.GetPortsAndIPAddress();
-            Console.WriteLine(Meeting.ipAddress + " : " + Meeting.port);
+            MeetingCredentials meeting = serverSM.GetPortsAndIPAddress();
+            Console.WriteLine(meeting.ipAddress + " : " + meeting.port);
             Console.WriteLine("Meeting has started by Host");
             while (true)
             {
