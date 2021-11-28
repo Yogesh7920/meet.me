730ddeb17c4c8dfc4cca3398de898ced2ee14f99
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2021-11-28 12:46:23 +0530

    Renamed some test files

--- Testing/Dashboard/SessionManagement/TestModules/FakeTelemetry.cs
+++ Testing/Dashboard/SessionManagement/TestModules/FakeTelemetry.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/// <author> Rajeev Goyal </author>
+/// <created> 24/112021 </created>
+/// <summary>
+/// This file contains the fake telemetr which subscribes and listens for the client session data and provide mock analytics.
+/// </summary>
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
